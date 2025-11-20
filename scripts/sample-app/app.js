6704f1ddb959a712b3487f45aad2b5e0b5ff6fde
desmazuv <volcy.desmazures@edu.esiee.fr>
2025-11-20 16:57:47 +0100

    Fix app response to match test

--- scripts/sample-app/app.js
+++ scripts/sample-app/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('DevOps Labs !!');
+  res.send('DevOps Labs!');
 });
 
 app.get('/name/:name', (req, res) => {
